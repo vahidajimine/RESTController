7f7aa0fa0da95e70819b8d351735c77b5d60d037
Vahid Ajimine <vahidajimine@Vahids-MacBook-Pro.local>
2018-02-12 12:20:17 +0100

    Fixed Test cases and changed the order of the parameters. Also made the params field optional. Also added in GET protocols

--- RESTController/RESTController.swift
+++ RESTController/RESTController.swift
@@ -32,14 +32,14 @@ class RESTController {
      
      Note: this function only currently works for the Content-Type `application/json`. Additional content types will
      
-     - parameter params: A dictionary of `key:value` parameters to be passed in the url rest call
      - parameter url: the string of the url for the server
      - parameter headers: A dictionary of `key:value` to be added to the http header call
+     - parameter params: A dictionary of `key:value` parameters to be passed in the url rest call
      - parameter method: either `POST` or `GET`. Default is POST
      - parameter contentType: only supports json and urlEncode currently. Default value is .json
      - parameter isSuccess: a string that is the key of the (key:value) in the JSON data result. It is usually "success"
      */
-    func restCall(params: [String: AnyObject], url: String, headers: [String:String]? = nil, method: HTTPMethod = HTTPMethod.post, contentType: ContentType = ContentType.json, isSuccess: String? = nil) {
+    func restCall(url: String, headers: [String:String]? = nil, params: [String: String] = [:], method: HTTPMethod = HTTPMethod.post, contentType: ContentType = ContentType.json, isSuccess: String? = nil) {
         
         //Checks to see if the URL string is valid
         guard let tempURL = URL(string: url) else {
@@ -59,42 +59,48 @@ class RESTController {
         }
         
         do {
-            //TODO: Add GET protocols
-            switch contentType {
-            case .json:
-                //Converts the params object to JSON in the HTTP request
-                let jsonPost = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions.prettyPrinted)
-                request.httpBody = jsonPost
-                request.addValue(ContentType.json.rawValue , forHTTPHeaderField: ContentType.headerFieldValue)
-            case .urlEncode:
-                //Convert the Dictionary to a String
-                var postValues: String = ""
-                for (key, value) in params{
-                    if postValues == ""{ //Initial run
-                        postValues += "\(key)=\(value)"
+            if (method == .post) {
+                switch contentType {
+                case .json:
+                    //Converts the params object to JSON in the HTTP request
+                    let jsonPost = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions.prettyPrinted)
+                    request.httpBody = jsonPost
+                    request.addValue(ContentType.json.rawValue , forHTTPHeaderField: ContentType.headerFieldValue)
+                case .urlEncode:
+                    //Convert the Dictionary to a String
+                    var postValues: String = ""
+                    for (key, value) in params{
+                        if postValues == ""{ //Initial run
+                            postValues += "\(key)=\(value)"
+                        }
+                        else { // Need to do have & in between each param
+                            postValues += "&\(key)=\(value)"
+                        }
                     }
-                    else { // Need to do have & in between each param
-                        postValues += "&\(key)=\(value)"
-                    }
+                    //Must encode the request to HTTPRequest
+                    request.httpBody = postValues.data(using: String.Encoding.utf8, allowLossyConversion: true)
+                    //Needed to let the Server know what kind of data that is being sent
+                    request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+                case .multipartForm:
+                    //TODO: Implement multipart/form-data
+                    print ("Not Implemented yet")
+                    return
+                case .textHTML:
+                    //TODO: Implement text/HTML
+                    print ("Not Implemented yet")
+                    return
                 }
-                //Must encode the request to HTTPRequest
-                request.httpBody = postValues.data(using: String.Encoding.utf8, allowLossyConversion: true)
-                //Needed to let the Server know what kind of data that is being sent
-                request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-            case .multipartForm:
-                //TODO: Implement multipart/form-data
-                print ("Not Implemented yet")
-                return
-            case .textHTML:
-                //TODO: Implement text/HTML
-                print ("Not Implemented yet")
-                return
+            } else { //GET
+                for (key,value) in params {
+                    request.addValue(value, forHTTPHeaderField: key)
+                }
             }
         } catch {
             self.runDelegateProtocolFunction(printString: "Error: could not convert params to JSON", error: "", url: url)
             return
         }
         
+        //MARK: After call
         let task = session.dataTask(with: request, completionHandler: {
             (data, response, error) -> Void in
             
@@ -111,11 +117,13 @@ class RESTController {
                 return
             }
             
-            let json:[String: AnyObject]!
+            let json:[String: AnyObject]
             
             //Try to convert the data stream to a dictionary
             do {
+                //print(resultData) //Debug print
                 json = try JSONSerialization.jsonObject(with: resultData, options: .mutableLeaves) as! [String: AnyObject]
+                //print(json) //Debug print
             } catch {
                 let jsonString = String(data: resultData, encoding: String.Encoding.utf8)
                 self.runDelegateProtocolFunction(printString: "Error could not parse JSON: '\(jsonString!)'", error: jsonString!, url: url)
@@ -150,10 +158,10 @@ class RESTController {
      This runs the delegate method `self.delegate.didReceiveAPIResults` and dispatches it to the main thread
      
      - parameter printString: the `String` to be printed to the console
-     - parameter results:       the results `[String: AnyObject]!` parameter for the function `RESTControllerProtocol.didRecieveAPIResults`
+     - parameter results:       the results `[String: AnyObject]` parameter for the function `RESTControllerProtocol.didRecieveAPIResults`
      - parameter url:         the url string parameter for the function `RESTControllerProtocol.didRecieveAPIResults`
      */
-    private func runDelegateProtocolFunction(printString: String, results: [String: AnyObject]!, url: String) {
+    private func runDelegateProtocolFunction(printString: String, results: [String: AnyObject], url: String) {
         print(printString)
         DispatchQueue.main.async {self.delegate?.didReceiveAPIResults(results: results, url: url) }
     }
--- RESTControllerTests/RESTControllerTests.swift
+++ RESTControllerTests/RESTControllerTests.swift
@@ -11,11 +11,10 @@ import XCTest
 
 class RESTControllerTests: XCTestCase {
     
-    let testClassDelegate = TestDelegate()
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
     
     override func tearDown() {
@@ -23,36 +22,74 @@ class RESTControllerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testRestCall() {
-        let testRestController = RESTController(delegate: testClassDelegate)
+    func testGetRestCall() {
+        let testDelegate = TestDelegate()
+        let testRestController = RESTController(delegate: testDelegate)
+        testDelegate.asyncExpectation = expectation(description: "JSON call with jsonplaceholder post call 1 return")
+        testRestController.restCall(url: "https://jsonplaceholder.typicode.com/posts/1", method: HTTPMethod.get)
         
-        self.testClassDelegate.asyncExpectation = expectation(description: "JSON call with jsonplaceholder post call 1 return")
-        
-        testRestController.restCall(params: [:], url: "https://jsonplaceholder.typicode.com/posts/1")
+        wait(for: [testDelegate.asyncExpectation!], timeout: 10)
+        if let error = testDelegate.errorString{
+            XCTFail("didNotReceiveAPIResults triggered with the following \(error)")
+            return
+        }
+        guard let result = testDelegate.results else {
+            XCTFail("Expected result to be successful")
+            return
+        }
+        XCTAssertTrue(result["title"] != nil, result.description)
+    }
+    
+    func testPostRestCall() {
+        let testDelegate = TestDelegate()
+        let testRestController = RESTController(delegate: testDelegate)
+        testDelegate.asyncExpectation = expectation(description: "JSON call with jsonplaceholder post call 1 return")
+        testRestController.restCall(url: "http://ip.jsontest.com/")
         
         waitForExpectations(timeout: 1, handler: { (testError) in
             if let error = testError{
                 XCTFail("waitForExpectations errored with \(error)")
             }
-            guard let errorString = self.testClassDelegate.errorString else {
+            if let error = testDelegate.errorString{
+                XCTFail("didNotReceiveAPIResults triggered with the following \(error)")
+                return
+            }
+            guard let result = testDelegate.results else {
+                XCTFail("Result is empty for testPostRestCal")
+                return
+            }
+            XCTAssertTrue(result["ip"] != nil, result.description)
+        })
+    }
+    
+    func testFailedRestCall(){
+        let testDelegate = TestDelegate()
+        let testRestController = RESTController(delegate: testDelegate)
+        testDelegate.asyncExpectation = expectation(description: "FlixBusTest")
+        testRestController.restCall(url: "http://bogus.url.com/notreal")
+        waitForExpectations(timeout: 10, handler: { (testError) in
+            if let error = testError{
+                XCTFail("waitForExpectations errored with \(error)")
+            }
+            if let error = testDelegate.results{
+                XCTFail("didReceiveAPIResults triggered with the following \(error.description)")
+                return
+            }
+            guard let correctResponse = testDelegate.errorString else {
                 XCTFail("Expected result to be successful")
                 return
             }
-            guard let result = self.testClassDelegate.results else {
-                XCTFail("Expected result to be successful")
-                return
-            }
-            print(result)
-            print(errorString)
-            XCTAssertTrue(true)
+            XCTAssertTrue(correctResponse == "", correctResponse)
         })
     }
     
     func testPerformanceExample() {
+        /*
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
+        // */
     }
     
     //MARK: - RESTControllerDelegate
